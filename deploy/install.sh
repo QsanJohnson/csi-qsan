0382500f366a9ebfbaecd8bb8ede0642875fd374
QsanJohnson <johnson.cheng@qsan.com>
2022-12-28 02:07:25 -0500

    1. Redundant controller plugin. 2. Volume metadata. 3. Self test code.

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-kubectl create ns qsan
-kubectl create secret -n qsan generic qsan-auth-secret --from-file=${BASE_DIR}/qsan-auth.yaml
+if kubectl get node --show-labels | grep -q topology.qsan.com/fc; then
+	kubectl label nodes --all topology.qsan.com/fc-
+fi
 
+kubectl create ns qsan
+#kubectl create secret -n qsan generic qsan-auth-secret
+kubectl create secret -n qsan generic qsan-auth-secret --from-file=${BASE_DIR}/qsan-auth.yaml
+#kubectl create secret -n qsan generic qsan-auth-secret --save-config --dry-run=client --from-file=./qsan-auth.yaml -o yaml | kubectl apply -f -
+
+#kubectl apply -f ${BASE_DIR}/secret.yaml
 kubectl apply -f ${BASE_DIR}/rbac.yaml
 kubectl apply -f ${BASE_DIR}/driverinfo.yaml
 kubectl apply -f ${BASE_DIR}/controller.yaml
--- deploy/uninstall.sh
+++ deploy/uninstall.sh
@@ -5,6 +5,9 @@ kubectl delete -f ${BASE_DIR}/node.yaml
 kubectl delete -f ${BASE_DIR}/controller.yaml
 kubectl delete -f ${BASE_DIR}/driverinfo.yaml
 kubectl delete -f ${BASE_DIR}/rbac.yaml
+kubectl delete -f ${BASE_DIR}/secret.yaml
 
-kubectl delete secret qsan-auth-secret -n qsan
-kubectl delete ns qsan
+#kubectl delete secret qsan-auth-secret -n qsan
+#kubectl delete ns qsan
+
+#kubectl label nodes --all topology.qsan.com/fc-
